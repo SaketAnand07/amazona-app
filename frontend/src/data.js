a70949c97ef84fec9d83be736964f5f11e36b38f
Saket Anand <saket.a2024@gmail.com>
2021-07-06 15:40:38 +0530

    first commit

---
+++ frontend/src/data.js
@@ -0,0 +1,73 @@
+const data = {
+    products: [
+        {
+            _id : 1,
+            name : 'Nike Slim Shirt',
+            category : 'shirts',
+            image : '/image/p1.jpg',
+            price : 120,
+            brand : 'Nike',
+            rating : 4.5,
+            numReviews: 10,
+            description : 'high quality product',
+
+        },
+        {
+            _id : 2,
+            name : 'adidas fit Shirt',
+            category : 'shirts',
+            image : '/image/p2.jpg',
+            price : 100,
+            brand : 'adodas',
+            rating : 4.0,
+            numReviews: 10,
+            description : 'high quality product',
+
+        },  {
+            _id : 3,
+            name : 'lacoste free Shirt',
+            category : 'shirts',
+            image : '/image/p3.jpg',
+            price : 220,
+            brand : 'lacoste',
+            rating : 4.8,
+            numReviews: 17,
+            description : 'high quality product',
+
+        },  {
+            _id : 4,
+            name : 'Nike Slim pants',
+            category : 'pants',
+            image : '/image/p4.jpg',
+            price : 78,
+            brand : 'Nike',
+            rating : 4.5,
+            numReviews: 14,
+            description : 'high quality product',
+
+        },  {
+            _id : 5,
+            name : 'puma Slim pant',
+            category : 'pants',
+            image : '/image/p5.jpg',
+            price : 65,
+            brand : 'puma',
+            rating : 4.5,
+            numReviews: 10,
+            description : 'high quality product',
+
+        },  {
+            _id : 6,
+            name : 'adidas fit pant',
+            category : 'pants',
+            image : '/image/p6.jpg',
+            price : 139,
+            brand : 'adidas',
+            rating : 4.5,
+            numReviews: 10,
+            description : 'high quality product',
+
+        },
+    ],
+};
+export default data;
